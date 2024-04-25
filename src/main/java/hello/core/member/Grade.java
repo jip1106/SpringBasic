b559145f0854ca04e7517e62905a55b2cdfc7495
jipark1106 <jipark1106@gmail.com>
2024-04-25 19:10:47 +0900

    review

--- src/main/java/hello/core/member/Grade.java
+++ src/main/java/hello/core/member/Grade.java
@@ -1,6 +1,12 @@
 package hello.core.member;
 
 public enum Grade {
-    BASIC,
-    VIP
+    BASIC("일반"),
+    VIP("VIP");
+
+    private String grade;
+
+    Grade(String grade){
+        this.grade = grade;
+    }
 }
